de15472bc9195e7a0ffef26f0cd03eef7471fa12
Cheri Hung <cherihung@gmail.com>
2021-01-18 11:30:14 -0500

    rm serveriWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,14 @@ import './index.css';
 import App from '@app/components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import whyDidYouRender from '@welldone-software/why-did-you-render';
+// import whyDidYouRender from '@welldone-software/why-did-you-render';
 
-import * as serviceWorker from './serviceWorker';
-
-if (import.meta.env.NODE_ENV === 'development') {
-  whyDidYouRender(React, {
-    onlyLogs: true,
-    include: [/^BaseWrapper/]
-  });
-}
+// if (import.meta.env.NODE_ENV === 'development') {
+//   whyDidYouRender(React, {
+//     onlyLogs: true,
+//     include: [/^BaseWrapper/]
+//   });
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
