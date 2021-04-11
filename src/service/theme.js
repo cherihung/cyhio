ab91705792bca32a3ebe1e0ec61b2bcd9da06962
Cheri Hung <cherihung@gmail.com>
2021-04-11 16:57:08 -0400

    finding

---
+++ src/service/theme.js
@@ -0,0 +1,27 @@
+import ThemeMap, { DEFAULT_THEME } from '@app/styles/themes';
+
+const LOCAL_STORAGE_KEY = 'cyh_theme';
+
+export const getUserTheme = () => {
+  const savedKey = localStorage.getItem(LOCAL_STORAGE_KEY);
+  const userTheme = getThemeObject(savedKey);
+
+  if (!userTheme) {
+    clearUserThemeKey();
+    return DEFAULT_THEME;
+  }
+  
+  return userTheme;
+}
+
+export const saveUserThemeKey = (themeKey) => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, themeKey);
+}
+
+export const clearUserThemeKey = () => {
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+export const getThemeObject = (key) => {
+  return ThemeMap[key];
+}
